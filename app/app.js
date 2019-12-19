4ef2093351807ed8d21b5f170083c956ab43ad1e
jmhammock <jason@arrayschool.com>
2019-12-19 11:07:11 -0700

    app.js with comments

--- app/app.js
+++ app/app.js
@@ -5,6 +5,20 @@
  * with document.createElement()
  **/ 
 
+/**
+ * 
+ * Product object using object constructor notation
+ * If you choose to define your methods on the prototype as I have,
+ * you need to be prepared to tell me why its best practice to do so.
+ * 
+ * @param {Number} id 
+ * @param {String} title 
+ * @param {String} description 
+ * @param {Number} price 
+ * @param {Number} quantityAvailable 
+ * @param {String} imgUrl 
+ * @param {String} creator 
+ */
 function Product(id, title, description, price, quantityAvailable, imgUrl, creator) {
     this.id = id;
     this.title = title;
@@ -27,6 +41,8 @@ Product.prototype.decreaseQuantity = function (number) {
 
 /**
  * Cart Model
+ * If you choose to define your methods on the prototype as I have,
+ * you need to be prepared to tell me why its best practice to do so.
  */
 function Cart() {
     this.lineItems = [];
@@ -74,6 +90,8 @@ Cart.prototype.removeProductFromCart = function (idToFind) {
 
 /**
  * MOCK DB & BUILD PRODUCTS
+ * Using object literal notation.
+ * Notice that methods are not defined on the prototype when using object literal notation
  */
 
 let Database = {
@@ -105,6 +123,11 @@ let Database = {
 
 let desc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
 
+/**
+ * Using a disposable array to auto create products. It is disposable because it is not named thus not stored
+ * in memory after it is used.
+ */
+
 [
     'Buffalo',
     'Jackrabbit',
@@ -118,7 +141,9 @@ let desc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius
 
 /**
  * CLONE, MODIFY & ADD NEW CARD FOR EACH PRODUCT
+ * Notice that I am operating directly on the Database.products array. 
  */
+
 Database.products.forEach(function(product){
     let template = document.getElementById('cardTemplate'),
         clone = document.importNode(template.content, true);
@@ -130,22 +155,12 @@ Database.products.forEach(function(product){
     article.querySelector('.content').innerText = product.description;
     article.querySelector('.card-footer-item').dataset.productId = product.id;
 
-    // article.addEventListener('click', function(event){
-    //     console.log('Hello from the article listener');
-    //     console.log(event.target);
-    // });
-
-    // article.querySelector('.card-footer-item').addEventListener('click', function(event){
-    //     // event.stopPropagation();
-    //     // console.log('Hello from the button listener');
-    //     // console.log(event.target);
-    // });
-
     document.getElementById('products').appendChild(article);
 });
 
 /**
  * Initialize new Cart
+ * Make sure to create a new cart. A cart does not exist until you instantiate it, meaning call 'new Cart()'
  */
 
 const CART = new Cart();
@@ -154,6 +169,19 @@ const CART = new Cart();
  * Events & Handlers
  */
 
+/**
+ * Custom event to be dispatched (or fired, or emitted; they mean the same thing) after we have added a product to
+ * our cart. 
+ */
+
+let addToCardEvent = new CustomEvent('addedToCart', {
+    bubbles: true
+});
+
+/**
+ * Event listener added to the main.products element that is listening for clicks on any
+ * a.card-footer-items that are children of main.products. This is an example of event delegation.
+ */
 document.getElementById('products').addEventListener('click', function (event){
     //Add Products To Cart
     if (event.target.matches('a.card-footer-item')) {
@@ -163,12 +191,11 @@ document.getElementById('products').addEventListener('click', function (event){
     }
 });
 
+/**
+ * Event listener added to the document that is listening for our custom event
+ */
 document.addEventListener('addedToCart', function(event){
     document.getElementById('counter').innerText = CART.lineItems.length;
-});
-
-let addToCardEvent = new CustomEvent('addedToCart', {
-    bubbles: true
 });
 
 
